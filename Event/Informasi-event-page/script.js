01758d71b7bfcd663b3f6035f7782d237fe9f86a
heykall <muh.heykall@gmail.com>
2023-10-19 22:16:28 +0700

    handle id for get detail

--- Event/Informasi-event-page/script.js
+++ Event/Informasi-event-page/script.js
@@ -1,3 +1,27 @@
+
+
+const url = new URLSearchParams(window.location.search);
+const idUrl = url.get('id');
+
+const containerEvent = document.getElementById('container-event')
+
+console.log(containerEvent);
+
+fetch('https://652d0acbf9afa8ef4b26af21.mockapi.io/event/' + idUrl )
+.then((response) => {
+  return response.json()
+})
+.then((data) => {
+  containerEvent.innerHTML = `
+  <img src="foto/sertifikasi${data.id}.png" alt="sertifikasi${data.id}" id="sertifikasi${data.id}"/>
+        <p>${data.desSertifikasi} 
+        </p>
+        <a href="https://www.loket.com/event/kelas-malam-digital-marketing-pelatihan-sertifikasi-profesi-durd">
+          <button type="button" id="informasi-event-button">Selengkapnya</button></a>
+  `
+})
+
+
 const kembali = document.getElementById("icon-back");
 const selengkapnya = document.getElementById("informasi-event-button");
 
@@ -15,5 +39,4 @@ selengkapnya.addEventListener("click", function() {
         selengkapnya.style.transform = "scale(1)";
     }, 300); 
 });
-
 
